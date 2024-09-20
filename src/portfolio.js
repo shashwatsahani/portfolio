a5699bb1dee781b7eff794795d9fae164c5cb2d3
shashwatsahani <shashwatsahani@Gmail.com>
2024-09-20 12:14:51 +0000

    Change

--- src/portfolio.js
+++ src/portfolio.js
@@ -21,7 +21,7 @@ const illustration = {
 
 const greeting = {
   username: "Shash",
-  title: "Hi all, I'm Shash
+  title: "Hi all, I'm Shash",
   subTitle: emoji(
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
   ),
